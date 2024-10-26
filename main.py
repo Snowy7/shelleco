7bf2c315941a95217baafdc99acb563d6c6fa202
Snowy <97126807+Snowy7@users.noreply.github.com>
2024-10-26 16:03:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-import cv2
-from picamera2 import Picamera2, Preview
-from time import sleep
-
-picam2 = Picamera2()
-
-# Start preview
-picam2.start_preview(Preview.QTGL)
-picam2.start()
-
-sleep(10)
-picam2.close()
+import gpiod
+import time
+LED_PIN = 17
+chip = gpiod.Chip('gpiochip4')
+led_line = chip.get_line(LED_PIN)
+led_line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
+try:
+   while True:
+       led_line.set_value(1)
+       time.sleep(1)
+       led_line.set_value(0)
+       time.sleep(1)
+finally:
+   led_line.release()
