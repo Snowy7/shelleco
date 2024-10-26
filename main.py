63b2aa1c24dadc48e3fab8f17528e2576e369ff0
Snowy <97126807+Snowy7@users.noreply.github.com>
2024-10-26 15:15:36 +0300

    camera test

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello world")
+from picamera2 import PiCamera2, Preview
+from time import sleep
+
+picam2 = PiCamera2()
+
+# Start preview
+picam2.start_preview(Preview.QTGL)
+picam2.start()
+
+sleep(10)
+picam2.close()
